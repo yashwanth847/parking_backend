cbc5bcb474947ee4ae8fd501c8f6248e0b24c2c9
nithin2889 <nithin2889@gmail.com>
2019-08-30 08:02:54 -0800

    final commited

--- src/main/java/com/hcl/parking/service/UserRegistrationServiceImpl.java
+++ src/main/java/com/hcl/parking/service/UserRegistrationServiceImpl.java
@@ -12,24 +12,29 @@ import org.springframework.stereotype.Service;
 import com.hcl.parking.dto.UserRegistrationRequestDto;
 import com.hcl.parking.dto.UserRegistrationResponseDto;
 import com.hcl.parking.entity.Registration;
+import com.hcl.parking.entity.Slot;
 import com.hcl.parking.exception.CommonException;
 import com.hcl.parking.repository.RegistrationRepository;
+import com.hcl.parking.repository.SlotRepository;
 import com.hcl.parking.util.ParkingConstants;
 import com.hcl.parking.util.PasswordUtil;
 
 @Service
-public class UserRegistrationServiceImpl implements UserRegistrationService{
+public class UserRegistrationServiceImpl implements UserRegistrationService {
 	private static Logger logger = LoggerFactory.getLogger(UserRegistrationServiceImpl.class);
-	
+
 	@Autowired
 	RegistrationRepository registrationRepository;
+
+	@Autowired
+	SlotRepository slotRepository;
 
 	/**
 	 * This method is intended to register user and provide a slot for user
 	 * 
 	 * @param UserRegistrationRequestDto is the input object which includes
-	 *                          password,mobileNumber,overAllExperience
-	 *                          userName, hclExperience;
+	 *                                   password,mobileNumber,overAllExperience
+	 *                                   userName, hclExperience;
 	 * @exception USER_ALREADY_REGISTERED,INVALID_MOBILENUMBER,INVALID_NAME
 	 * @return UserRegistrationResponseDto which returns message
 	 */
@@ -39,30 +44,28 @@ public class UserRegistrationServiceImpl implements UserRegistrationService{
 		UserRegistrationResponseDto response = new UserRegistrationResponseDto();
 		Registration register = new Registration();
 		PasswordUtil passwordUtil = new PasswordUtil();
-		Optional<Registration> registration = registrationRepository.findByMobileNumber(userRegistrationRequestDto.getMobileNumber());
-		if(registration.isPresent())
+		Optional<Registration> registration = registrationRepository
+				.findByMobileNumber(userRegistrationRequestDto.getMobileNumber());
+		if (registration.isPresent())
 			throw new CommonException(ParkingConstants.USER_ALREADY_REGISTERED);
-		if(!validMobileNumber(userRegistrationRequestDto.getMobileNumber()))
-		{
+		if (!validMobileNumber(userRegistrationRequestDto.getMobileNumber())) {
 			throw new CommonException(ParkingConstants.INVALID_MOBILENUMBER);
 		}
-		
-		if(!validateCustomerName(userRegistrationRequestDto.getUserName()))
-		{
+
+		if (!validateCustomerName(userRegistrationRequestDto.getUserName())) {
 			throw new CommonException(ParkingConstants.INVALID_NAME);
 		}
-		
-		if(userRegistrationRequestDto.getOverAllExperience()>=15 && userRegistrationRequestDto.getHclExperience()>=5)
-		{
+
+		if (userRegistrationRequestDto.getOverAllExperience() >= 15
+				&& userRegistrationRequestDto.getHclExperience() >= 5) {
 			register.setMobileNumber(userRegistrationRequestDto.getMobileNumber());
 			register.setHclExperience(userRegistrationRequestDto.getHclExperience());
 			register.setOverAllExperience(userRegistrationRequestDto.getOverAllExperience());
 			register.setPassword(passwordUtil.encodePassword(userRegistrationRequestDto.getPassword()));
 			register.setRoleId(2);
 			register.setUserName(userRegistrationRequestDto.getUserName());
-		}
-		else 
-		{
+
+		} else {
 			register.setMobileNumber(userRegistrationRequestDto.getMobileNumber());
 			register.setHclExperience(userRegistrationRequestDto.getHclExperience());
 			register.setOverAllExperience(userRegistrationRequestDto.getOverAllExperience());
@@ -70,8 +73,17 @@ public class UserRegistrationServiceImpl implements UserRegistrationService{
 			register.setRoleId(3);
 			register.setUserName(userRegistrationRequestDto.getUserName());
 		}
-			
-		registrationRepository.save(register);
+
+		Registration reg = registrationRepository.save(register);
+
+		if (userRegistrationRequestDto.getOverAllExperience() >= 15
+				&& userRegistrationRequestDto.getHclExperience() >= 5) {
+			Slot slot = new Slot();
+			slot.setSlotName("A" + reg.getRegistrationId());
+			slot.setRegistrationId(reg.getRegistrationId());
+			slotRepository.save(slot);
+		}
+
 		response.setMessage("User registered successfuly");
 		return response;
 	}
