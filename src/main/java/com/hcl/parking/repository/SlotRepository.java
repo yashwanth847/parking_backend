e4b94dcdc64c98f8a0fd964c7df6708ae2b42a16
Gurpreet Singh Gohir <gurpreet.gohir@gmail.com>
2019-08-30 19:01:19 +0530

    gurpreet

--- src/main/java/com/hcl/parking/repository/SlotRepository.java
+++ src/main/java/com/hcl/parking/repository/SlotRepository.java
@@ -2,6 +2,7 @@ package com.hcl.parking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.hcl.parking.entity.Slot;
 
 @Repository
